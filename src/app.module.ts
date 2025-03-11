5868cd9ec6b13b8e68fe3afb133725b3c147a3df
Bryan Christy <bryanchristy2016@gmail.com>
2025-03-11 19:42:27 +0530

    BUILD: User SQL Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgreSQLConfig } from './common/config/pg.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => PostgreSQLConfig,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
