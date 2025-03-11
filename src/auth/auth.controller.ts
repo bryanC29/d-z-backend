295045608e11e97e86edc4d255c262b6739b6d5b
bryanC29 <bryanchristy2016@gmail.com>
2025-03-11 10:27:03 +0530

    BUILD: Auth path

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login() {
+    return 'login';
+  }
+
+  @Post('register')
+  register() {
+    return 'register';
+  }
+
+  @Get('logout')
+  logout() {
+    return 'logout';
+  }
+
+  @Get('forgot-password')
+  forgotPassword() {
+    return 'forgot-password';
+  }
+
+  @Post('reset-password')
+  resetPassword() {
+    return 'reset-password';
+  }
+}
